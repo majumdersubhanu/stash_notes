783048fc19d12c8bb1ad4f3555a39462ff66ee5d
Subhanu Majumder <reach.subhanu@gmail.com>
2025-04-19 22:01:52 +0530

    feat: offline storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +10,10 @@ import 'package:stash_notes/screens/login_screen.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
+  );
 
   final currUser = FirebaseAuth.instance.currentUser;
 
