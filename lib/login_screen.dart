1cfeb8811397306dd663519b1e4b6c591b60561a
Subhanu Majumder <reach.subhanu@gmail.com>
2025-04-19 12:51:13 +0530

    feat: form validation

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -9,6 +9,11 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,125 +31,157 @@ class _LoginScreenState extends State<LoginScreen> {
             child: ConstrainedBox(
               constraints: BoxConstraints(maxWidth: 350),
               child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Login',
-                      style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Welcome back to StashNotes',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    SizedBox(height: 48),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Email address',
-                        hintText: 'john.doe@xyz.com',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        prefixIcon: Icon(Icons.alternate_email),
-                      ),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        prefixIcon: Icon(Icons.password),
-                        suffixIcon: Icon(Icons.visibility),
-                      ),
-                    ),
-                    SizedBox(height: 32),
-                    FilledButton(
-                      onPressed: () {},
-                      child: Text('Continue with email'),
-                    ),
-                    SizedBox(height: 16),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text('Forgot password'),
-                    ),
-                    SizedBox(height: 8),
-                    Row(
-                      spacing: 16,
-                      children: [
-                        Expanded(child: Divider()),
-                        Text('OR'),
-                        Expanded(child: Divider()),
-                      ],
-                    ),
-                    SizedBox(height: 24),
-                    GestureDetector(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          color: Color(0xFF4285F4),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: Row(
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(13),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Image.asset(
-                                    'assets/icons/google.png',
-                                    height: 32,
-                                    width: 32,
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                child: Align(
-                                  alignment: Alignment.center,
-                                  child: Text(
-                                    'Continue with Google',
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 16),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => RegisterScreen(),
-                          ),
-                        );
-                      },
-                      child: Text(
-                        'Create an account',
+                child: Form(
+                  autovalidateMode: AutovalidateMode.onUnfocus,
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Login',
                         style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          decorationColor:
-                              Theme.of(context).colorScheme.primary,
-                          decorationThickness: 1.5,
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 8),
+                      Text(
+                        'Welcome back to StashNotes',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 48),
+                      TextFormField(
+                        controller: _emailController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter an email';
+                          }
+
+                          final bool emailValid = RegExp(
+                            r"^[a-zA-Z0-9.a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+                          ).hasMatch(value);
+
+                          if (!emailValid) {
+                            return 'Please enter a valid email';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Email address',
+                          hintText: 'john.doe@xyz.com',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          prefixIcon: Icon(Icons.alternate_email),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      TextFormField(
+                        controller: _passwordController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a password';
+                          }
+                          return null;
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          prefixIcon: Icon(Icons.password),
+                          suffixIcon: Icon(Icons.visibility),
+                        ),
+                      ),
+                      SizedBox(height: 32),
+                      FilledButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('WIP')),
+                            );
+                          }
+                        },
+                        child: Text('Continue with email'),
+                      ),
+                      SizedBox(height: 16),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text('Forgot password'),
+                      ),
+                      SizedBox(height: 8),
+                      Row(
+                        spacing: 16,
+                        children: [
+                          Expanded(child: Divider()),
+                          Text('OR'),
+                          Expanded(child: Divider()),
+                        ],
+                      ),
+                      SizedBox(height: 24),
+                      GestureDetector(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            color: Color(0xFF4285F4),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Row(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(13),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Image.asset(
+                                      'assets/icons/google.png',
+                                      height: 32,
+                                      width: 32,
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      'Continue with Google',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RegisterScreen(),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          'Create an account',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            decorationColor:
+                                Theme.of(context).colorScheme.primary,
+                            decorationThickness: 1.5,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -8,6 +8,13 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,136 +32,189 @@ class _RegisterScreenState extends State<RegisterScreen> {
             child: ConstrainedBox(
               constraints: BoxConstraints(maxWidth: 350),
               child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Register',
-                      style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Register and STASH away your notes',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    SizedBox(height: 48),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Email address',
-                        hintText: 'john.doe@xyz.com',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
+                child: Form(
+                  autovalidateMode: AutovalidateMode.onUnfocus,
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Register',
+                        style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
                         ),
-                        prefixIcon: Icon(Icons.alternate_email),
                       ),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        helper: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('At least 8 characters'),
-                            Text('1 upper case letter'),
-                            Text('1 lower case letter'),
-                            Text('1 special character'),
-                            Text('1 number'),
-                          ],
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        prefixIcon: Icon(Icons.password),
-                        suffixIcon: Icon(Icons.visibility),
+                      SizedBox(height: 8),
+                      Text(
+                        'Register and STASH away your notes',
+                        style: TextStyle(fontSize: 16),
                       ),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: 'Confirm password',
+                      SizedBox(height: 48),
+                      TextFormField(
+                        controller: _emailController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter an email';
+                          }
 
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
+                          final bool emailValid = RegExp(
+                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+                          ).hasMatch(value);
+
+                          if (!emailValid) {
+                            return 'Please enter a valid email';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Email address',
+                          hintText: 'john.doe@xyz.com',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          prefixIcon: Icon(Icons.alternate_email),
                         ),
-                        prefixIcon: Icon(Icons.password),
                       ),
-                    ),
-                    SizedBox(height: 32),
-                    FilledButton(
-                      onPressed: () {},
-                      child: Text('Continue with email'),
-                    ),
-                    SizedBox(height: 24),
-                    Row(
-                      spacing: 16,
-                      children: [
-                        Expanded(child: Divider()),
-                        Text('OR'),
-                        Expanded(child: Divider()),
-                      ],
-                    ),
-                    SizedBox(height: 24),
-                    GestureDetector(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          color: Color(0xFF4285F4),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: Row(
+                      SizedBox(height: 16),
+                      TextFormField(
+                        controller: _passwordController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a password';
+                          }
+                          if (value.length < 8) {
+                            return 'Password must be at least 8 characters';
+                          }
+                          if (!RegExp(r'[A-Z]').hasMatch(value)) {
+                            return 'Password must contain at least one uppercase letter';
+                          }
+                          if (!RegExp(r'[a-z]').hasMatch(value)) {
+                            return 'Password must contain at least one lowercase letter';
+                          }
+                          if (!RegExp(
+                            r'[!@#$%^&*(),.?":{}|<>]',
+                          ).hasMatch(value)) {
+                            return 'Password must contain at least one special character';
+                          }
+                          if (!RegExp(r'\d').hasMatch(value)) {
+                            return 'Password must contain at least one number';
+                          }
+                          return null;
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          helper: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(13),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(4.0),
-                                  child: Image.asset(
-                                    'assets/icons/google.png',
-                                    height: 32,
-                                    width: 32,
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                child: Align(
-                                  alignment: Alignment.center,
-                                  child: Text(
-                                    'Continue with Google',
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                ),
-                              ),
+                              Text('8 characters'),
+                              Text('1 upper case letter'),
+                              Text('1 lower case letter'),
+                              Text('1 special character'),
+                              Text('1 number'),
                             ],
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          prefixIcon: Icon(Icons.password),
+                          suffixIcon: Icon(Icons.visibility),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      TextFormField(
+                        controller: _confirmPasswordController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter the password again';
+                          }
+                          if (value != _passwordController.text) {
+                            return 'Passwords do not match';
+                          }
+                          return null;
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Confirm password',
+
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          prefixIcon: Icon(Icons.password),
+                        ),
+                      ),
+                      SizedBox(height: 32),
+                      FilledButton(
+                        onPressed: () {},
+                        child: Text('Continue with email'),
+                      ),
+                      SizedBox(height: 24),
+                      Row(
+                        spacing: 16,
+                        children: [
+                          Expanded(child: Divider()),
+                          Text('OR'),
+                          Expanded(child: Divider()),
+                        ],
+                      ),
+                      SizedBox(height: 24),
+                      GestureDetector(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            color: Color(0xFF4285F4),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Row(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(13),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Image.asset(
+                                      'assets/icons/google.png',
+                                      height: 32,
+                                      width: 32,
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      'Continue with Google',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 16),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.maybePop(context);
-                      },
-                      child: Text(
-                        'Existing user? Login here',
-                        style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          decorationColor:
-                              Theme.of(context).colorScheme.primary,
-                          decorationThickness: 1.5,
+                      SizedBox(height: 16),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.maybePop(context);
+                        },
+                        child: Text(
+                          'Existing user? Login here',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            decorationColor:
+                                Theme.of(context).colorScheme.primary,
+                            decorationThickness: 1.5,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
